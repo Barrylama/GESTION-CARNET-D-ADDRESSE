192c45f65285ed47fb59f0b7f07b25d252f91506
Barrylama <barrylamarana170@gmail.com>
2023-09-29 15:52:03 +0000

    charge

--- client/src/features/auth/dashboard.jsx
+++ client/src/features/auth/dashboard.jsx
@@ -1,51 +1,54 @@
 import React, { useEffect, useState } from "react";
 import { useCountQuery } from "../../api/auth";
-import {useCountContactQuery} from "../../api/contact";
+import { useCountContactQuery } from "../../api/contact";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 function Dashboard() {
   const userContacts = 250; // Remplacez par le nombre de contacts de l'utilisateur connecté
-  const {data, isLoading, isSuccess, isError, } = useCountQuery(); // Remplacez par la fonction appropriée depuis votre API
-  const {data:dataCount, isLoadingCount, isSuccessCount, isErrorCount} = useCountContactQuery();
+  const { data, isLoading, isSuccess, isError } = useCountQuery(); // Remplacez par la fonction appropriée depuis votre API
+  const { data: dataCount, isLoading: isLoadingCount, isSuccess: isSuccessCount, isError: isErrorCount } = useCountContactQuery();
 
   return (
     <div>
       {/* Section pour le nombre total d'utilisateurs */}
-      <section className="py-10 m-5 flex rounded-xl" style={{
-        background: "linear-gradient(to right,#a2e0b9, #f0f0f0,#cccccc)"
-      }}>
-        <div className="w-1/2 mt-5 ">
+      <section
+        className="py-10 m-5 flex rounded-xl"
+        style={{
+          background: "linear-gradient(to right,#a2e0b9, #f0f0f0,#cccccc)",
+        }}
+      >
+        <div className="w-1/2 mt-5">
           {/* Carte pour le nombre total d'utilisateurs */}
           <div className="bg-white p-4 rounded-lg w-2/3 ml-10 shadow-md">
             <h2 className="text-3xl font-bold mb-4">Nombre total d'utilisateurs</h2>
-            {data?.totalUsers !== null ? (
-              <p className="text-6xl text-center font-bold rounded-md pt-5 pb-5" style={{
-               background: "#a8e1bd"
-             }}>{data?.totalUsers}</p>
+            {isLoading ? (
+              <div className="text-center">
+                <p className="text-2xl">Chargement en cours...</p>
+                <FontAwesomeIcon icon={faSpinner} spin />
+              </div>
             ) : (
-               <>
-               <span className="mr-2">Chargement du nombre de contact en cours ......</span>
-               <FontAwesomeIcon icon={faSpinner} spin />
-             </>
+              <p className="text-6xl text-center font-bold rounded-md pt-5 pb-5" style={{ background: "#a8e1bd" }}>
+                {data?.totalUsers}
+              </p>
             )}
           </div>
           {/* Carte pour le nombre de contacts de l'utilisateur connecté */}
           <div className="bg-white p-4 rounded-lg w-2/3 ml-10 mt-5 shadow-md">
-            <h2 className="text-3xl font-bold mb-4">Contacts enregistrés</h2>
-            {dataCount?.count !== null ? (
-              <p className="text-6xl text-white text-center font-bold rounded-md pt-5 pb-5" style={{
-               background: "#ff6581"
-             }}>{dataCount?.count}</p>
+            <h2 className="text-3xl font-bold mb-4 text-center">Vos contacts enregistrés</h2>
+            {isLoadingCount ? (
+              <div className="text-center">
+                <p className="text-2xl">Chargement en cours...</p>
+                <FontAwesomeIcon icon={faSpinner} spin />
+              </div>
             ) : (
-               <>
-               <span className="mr-2">Chargement du nombre de contact en cours ......</span>
-               <FontAwesomeIcon icon={faSpinner} spin />
-             </>
+              <p className="text-6xl text-white text-center font-bold rounded-md pt-5 pb-5" style={{ background: "#ff6581" }}>
+                {dataCount?.count}
+              </p>
             )}
           </div>
         </div>
-        <div className="w-1/2 mr-5 ">
+        <div className="w-1/2 mr-5">
           {/* Illustration à droite */}
           <img
             src="/src/assets/images/stats1.png"
